2d1f6780f9f40fb1d52f2c768dba77ce3fef74aa
Brindashree C B <brindashree@users.noreply.github.com>
2021-04-25 14:29:13 +0530

    connected to firebase

--- src/App.js
+++ src/App.js
@@ -1,11 +1,33 @@
-import logo from './logo.svg';
+
 import './App.css';
 import React from 'react';
+const firebase = require('firebase');
 
 class App extends React.Component {
+
+  constructor() {
+    super();
+    this.state = {
+      selectedNoteIndex: null,
+      selectedNote: null,
+      notes:null
+    }
+  }
   render() {
     return (<div>Hello world</div>);
   }
+}
+
+componentDidMount = () => {
+  firebase.firestore().collection('notes').onSnapshot(serverUpdate => {
+    const notes = serverUpdate.docs.map(_doc => {
+      const data = _doc.data();
+      data['id'] = _doc.id;
+      return data;
+    });
+    console.log(notes)
+    this.setState({notes:notes})
+  });
 }
 
 export default App;
