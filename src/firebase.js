4c934d6eb46c27f8351c75c429ed4ded738ee60f
Brindashree C B <brindashree@users.noreply.github.com>
2021-04-25 13:58:49 +0530

    initial commit

---
+++ src/firebase.js
@@ -0,0 +1,13 @@
+const firebase = require('firebase');
+require('firebase/firestore');
+
+var firebaseConfig = {
+    apiKey: "AIzaSyD4vh1IrQAehghxD7ZIu6-Sx8pilVqkMyc",
+    authDomain: "evernoteclone-377e1.firebaseapp.com",
+    projectId: "evernoteclone-377e1",
+    storageBucket: "evernoteclone-377e1.appspot.com",
+    messagingSenderId: "399365598045",
+    appId: "1:399365598045:web:43e4c1aeebcfaef24d56b4"
+  };
+  
+  firebase.initializeApp(firebaseConfig);
