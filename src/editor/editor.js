caa0505298ad765a6f33e2aa663f34316779a3e8
Brindashree C B <brindashree@users.noreply.github.com>
2021-04-27 11:44:00 +0530

    updates to db

--- src/editor/editor.js
+++ src/editor/editor.js
@@ -4,14 +4,37 @@ import debounce from '../helpers';
 import BorderColorIcon from '@material-ui/icons/BorderColor';
 import { withStyles } from '@material-ui/core/styles';
 import styles from './styles';
+import { ContactSupportOutlined } from '@material-ui/icons';
 
 class EditorComponent extends React.Component {
     constructor() {
         super();
+        this.state = {
+            text: '',
+            title: '',
+            id: ''
+        };
     }
     render() {
-        return(<div>hello</div>)
+        
+        
+        const { classes } = this.props;
+
+        return (
+            <div className={classes.editorContainer}>
+                <ReactQuill
+                    value={this.state.text} onChange={this.updateBody}
+                ></ReactQuill>
+            </div>
+        )
     }
+    updateBody = async (val) => {
+        await this.setState({ text: val });
+        this.update();
+    };
+    update = debounce(() => {
+        console.log('updating database');
+    })
 }
 
 export default withStyles(styles)(EditorComponent)
